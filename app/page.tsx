9ac84529e247264f46f1f7d5df0359d1cfccc4f8
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-13 22:22:50 +0200

    add intro section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Intro from "@/components/intro";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <h1>hello</h1> */}
+    <main className="flex flex-col items-center justify-between px-4 p-24">
+      <Intro />
     </main>
   );
 }
