f9d706e312bcdf3aa08bd9d96609bcba5f478888
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-14 00:18:07 +0200

    improve some styles

--- components/intro.tsx
+++ components/intro.tsx
@@ -26,12 +26,12 @@ export default function Intro() {
               src={img}
               alt="my photo"
               priority={true}
-              className="h-24 w-24 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+              className="h-56 w-56 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
             />
           </motion.div>
 
           <motion.span
-            className="absolute bottom-0 right-0 text-4xl"
+            className="absolute bottom-0 right-0 text-5xl"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{
