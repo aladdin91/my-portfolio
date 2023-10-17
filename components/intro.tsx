fa9cc5816d4df5c54edcea8b5b19d696a9bbd5fb
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-17 23:11:05 +0200

    add project section

--- components/intro.tsx
+++ components/intro.tsx
@@ -26,7 +26,7 @@ export default function Intro() {
               src={img}
               alt="my photo"
               priority={true}
-              className="h-56 w-56 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
+              className="h-48 w-48 rounded-full border-[0.35rem] border-white object-cover shadow-xl"
             />
           </motion.div>
 
@@ -45,8 +45,8 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
-      <motion.p
-        className=" mb-10 mt-4 px-4 text-3xl font-medium !leading-[1.5] sm:text-4xl"
+      <motion.h1
+        className=" mb-10 mt-4 px-4 text-1xl font-medium !leading-[1.5] sm:text-4xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
@@ -54,7 +54,7 @@ export default function Intro() {
         <span className="font-bold">full-stack developer</span> I enjoy building{" "}
         <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js) Typescript</span>.
-      </motion.p>
+      </motion.h1>
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
