e2cf75a7166c295ff6908bf1064e8630ee056070
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-18 16:09:13 +0200

    add the projects details and styles

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import Image from "next/image";
 type ProjectsProps = (typeof projectsData)[number];
 export default function Projects() {
   return (
@@ -18,5 +19,28 @@ export default function Projects() {
 }
 
 function Project({ title, description, tags, imageUrl }: ProjectsProps) {
-  return <div>{title}</div>;
+  return (
+    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]">
+      <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
+        <h3 className="text-2x1">{title}</h3>
+        <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
+        <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+          {tags.map((tag, index) => (
+            <li
+              className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full"
+              key={index}
+            >
+              {tag}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <Image
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2x1"
+        src={imageUrl}
+        alt={description}
+        quality={95}
+      />
+    </section>
+  );
 }
