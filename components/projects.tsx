9911e38568cb3fa7c68fbf4f46cb0c040f244db3
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-18 22:13:31 +0200

    improve the projects components styles

--- components/projects.tsx
+++ components/projects.tsx
@@ -20,8 +20,8 @@ export default function Projects() {
 
 function Project({ title, description, tags, imageUrl }: ProjectsProps) {
   return (
-    <section className="bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]">
-      <div className="pt-4 pb-8 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full">
+    <section className="group bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 sm:mb-8 last:mb-0 even:pl-8 hover:bg-gray-200 transition">
+      <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
         <h3 className="text-2x1">{title}</h3>
         <p className="mt-2 leading-relaxed text-gray-700">{description}</p>
         <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -36,7 +36,14 @@ function Project({ title, description, tags, imageUrl }: ProjectsProps) {
         </ul>
       </div>
       <Image
-        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2x1"
+        className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2x1 group-even:right-[initial] group-even:-left-40 transition 
+        group-hover:scale-[1.04]
+        group-hover:-translate-x-3
+        group-hover:-translate-y-3 group-hover:-rotate-2 
+        group-even:group-hover:translate-x-3
+        group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 
+        
+        "
         src={imageUrl}
         alt={description}
         quality={95}
