eaf61954305a86ba1d8edeb5d03edf64d025c080
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-23 21:40:27 +0200

    add animation to contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,12 +1,30 @@
+"use client";
 import React from "react";
 import SectionHeading from "./section-heading";
 import { FaPaperPlane } from "react-icons/fa";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
 
 export default function Contact() {
+  const { ref } = useSectionInView("Contact");
+
   return (
-    <section
+    <motion.section
+      ref={ref}
       id="contact"
-      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center scroll-mt-28"
+      initial={{
+        opacity: 0,
+      }}
+      whileInView={{
+        opacity: 1,
+      }}
+      transition={{
+        duration: 1,
+      }}
+      viewport={{
+        once: true,
+      }}
     >
       <SectionHeading>Contact me</SectionHeading>
       <p className="text-gray-700">
@@ -34,6 +52,6 @@ export default function Contact() {
           <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />{" "}
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
--- components/intro.tsx
+++ components/intro.tsx
@@ -9,10 +9,11 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home");
-
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
   return (
     <section
       ref={ref}
@@ -73,6 +74,10 @@ export default function Intro() {
         <Link
           href="#contact"
           className=" group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
           <BsArrowRight className=" opacity-70 group-hover:translate-x-1 transition" />
@@ -80,7 +85,7 @@ export default function Intro() {
         <a
           href="/Alaa-Abdelbaki-CV.pdf"
           download
-          className="group bg-white text-gray-700 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          className="group bg-white text-gray-700 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
         >
           Download CV{" "}
           <HiDownload className="opacity-70 group-hover:translate-y-1 transition" />
@@ -88,14 +93,14 @@ export default function Intro() {
         <a
           href="https://www.linkedin.com/in/alaa-abdelbaki/"
           target="_blank"
-          className=" bg-white text-gray-700 p-4 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:text-gray-900 hover:scale-[1.15] active:scale-[1.15] transition cursor-pointer border border-black/10"
+          className=" bg-white text-gray-700 p-4 py-3 flex items-center gap-2 rounded-full focus:scale-110 hover:text-gray-900 hover:scale-[1.15] active:scale-[1.15] transition cursor-pointer borderBlack"
         >
           <BsLinkedin />
         </a>
         <a
           href="https://github.com/aladdin91"
           target="_blank"
-          className=" bg-white text-gray-700 p-4  text-[1.35rem]  py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-900 active:scale-105 transition cursor-pointer border border-black/10"
+          className=" bg-white text-gray-700 p-4  text-[1.35rem]  py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-900 active:scale-105 transition cursor-pointer borderBlack"
         >
           <FaGithubSquare />
         </a>
