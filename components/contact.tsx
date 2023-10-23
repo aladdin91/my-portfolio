3d06578c047761225c40f318f91e11c284704f49
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-23 15:07:38 +0200

    add styles to contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,7 +4,10 @@ import { FaPaperPlane } from "react-icons/fa";
 
 export default function Contact() {
   return (
-    <section id="contact" className="mb-20 sm:mb-28 w-[min(100%,38rem)]">
+    <section
+      id="contact"
+      className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+    >
       <SectionHeading>Contact me</SectionHeading>
       <p className="text-gray-700">
         Please contact me directly at{" "}
@@ -14,13 +17,21 @@ export default function Contact() {
         or through this form{" "}
       </p>
       <form className="mt-10 flex flex-col">
-        <input className="h-14 rounded-lg borderBlack" type="email" />
-        <textarea className="h-52 my-3 rounded-lg borderBlack p-4" />
+        <input
+          placeholder="Your email"
+          className="h-14 px-4 rounded-lg borderBlack"
+          type="email"
+        />
+        <textarea
+          placeholder="Your message"
+          className="h-52 my-3 rounded-lg borderBlack p-4"
+        />
         <button
           type="submit"
-          className="flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all"
+          className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 action:scale-105 hover:bg-gray-950"
         >
-          Submit <FaPaperPlane className="text-xs opacity-70 transition-all" />{" "}
+          Submit{" "}
+          <FaPaperPlane className="text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1 " />{" "}
         </button>
       </form>
     </section>
