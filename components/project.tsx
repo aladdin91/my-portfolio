89ec8eea4ac0c6185cccfbf379c088d996f5f9cf
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-26 23:24:40 +0200

    make project component responsive

--- components/project.tsx
+++ components/project.tsx
@@ -28,8 +28,8 @@ export default function Project({
         opacity: opacityProgress,
       }}
     >
-      <section className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  last:mb-0 even:pl-8 hover:bg-gray-200 transition rounded-lg group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      <section className=" bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden sm:pr-8 relative sm:h-[20rem]  last:mb-0 even:pl-8 hover:bg-gray-200 transition rounded-lg sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2x1">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -46,7 +46,7 @@ export default function Project({
           </ul>
         </div>
         <Image
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2x1 group-even:right-[initial] group-even:-left-40 transition 
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2x1 group-even:right-[initial] group-even:-left-40 transition 
         group-hover:scale-[1.04]
         group-hover:-translate-x-3
         group-hover:-translate-y-3 group-hover:-rotate-2 
