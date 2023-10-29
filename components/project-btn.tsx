c5b4d82bb9fa24eefa0db95caada3ab3e9155e49
Alaa Abdelbaki <a.eldeen91@gmail.com>
2023-10-29 15:57:34 +0100

    remove unwanted styles

--- components/project-btn.tsx
+++ components/project-btn.tsx
@@ -8,7 +8,7 @@ export default function ProjectBtn({ url, text }: ProjectBtnProps) {
   return (
     <a
       href={url}
-      className="flex items-center justify-center gap-2 h-[2.3rem] w-[8rem] bg-black/[0.7]  text-white rounded-xl outline-none transition-all focus:scale-110 hover:bg-gray-950 active:scale-105  disabled:scale-100 disabled:bg-opacity-65 dark:bg-gray-950 dark:text-white/70"
+      className="flex items-center justify-center gap-2 h-[2.3rem] w-[8rem] bg-black/[0.7]  text-white rounded-xl transition-all focus:scale-110 hover:bg-gray-950 active:scale-105  dark:bg-gray-950 dark:text-white/70"
     >
       {text}
     </a>
